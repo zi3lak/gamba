9e177d23284ad432daffa88c254e2ccd5cf577f2
zi3lak <zi3lak@gmail.com>
2024-02-09 18:26:31 +0000

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,52 @@
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import '@solana/wallet-adapter-react-ui/styles.css'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { GambaPlatformProvider } from 'gamba-react-ui-v2'
+import { GambaProvider } from 'gamba-react-v2'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter} from 'react-router-dom'
+import App from './App'
+import { PLATFORM_CREATOR_ADDRESS, RPC_ENDPOINT, TOKENS } from './constants'
+import { GAMES } from './games'
+import './styles.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+function Root() {
+  const wallets = React.useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ],
+    [],
+  )
+
+  return (
+    <BrowserRouter>
+      <ConnectionProvider
+        endpoint={RPC_ENDPOINT}
+        config={{ commitment: 'processed' }}
+      >
+        <WalletProvider autoConnect wallets={wallets}>
+          <WalletModalProvider>
+            <GambaProvider>
+              <GambaPlatformProvider
+                creator={PLATFORM_CREATOR_ADDRESS}
+                games={GAMES}
+                tokens={TOKENS}
+                defaultCreatorFee={0.01}
+                defaultJackpotFee={0.001}
+              >
+                <App />
+              </GambaPlatformProvider>
+            </GambaProvider>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </BrowserRouter>
+  )
+}
+
+root.render(<Root />)
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+const ENV_PREFIX = ['VITE_']
+
+export default defineConfig(() => ({
+  envPrefix: ENV_PREFIX,
+  server: { port: 4001, host: false },
+  assetsInclude: ["**/*.glb"],
+  define: {
+    'process.env.ANCHOR_BROWSER': true,
+    'global': 'window',
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
+  plugins: [
+    react({ jsxRuntime: 'classic' }),
+  ],
+}))
